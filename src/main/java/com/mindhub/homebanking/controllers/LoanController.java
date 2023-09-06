24c27663cfb1af9ecee5828f8d4741c1ffda2ae3
Nacho <ignacio.millan1986@gmail.com>
2023-09-06 12:08:58 -0300

    task9

--- src/main/java/com/mindhub/homebanking/controllers/LoanController.java
+++ src/main/java/com/mindhub/homebanking/controllers/LoanController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -35,33 +36,42 @@ public class LoanController {
         return loanRepository.findAll().stream().map(loan -> new LoanDTO(loan)).collect(Collectors.toList());
     }
 
+    @Transactional
     @PostMapping("/loans")
     public ResponseEntity<Object>loanApplication(@RequestBody LoanApplicationDTO loanApplicationDTO, Authentication authentication){
         Client client = clientRepository.findByEmail(authentication.getName());
         Loan loan = loanRepository.findById(loanApplicationDTO.getLoanId());
         Account account = accountRepository.findByNumber(loanApplicationDTO.getToAccountNumber());
 
+        //Verificar que los parametros no esten vacios
         if (loanApplicationDTO.toString().isBlank()) { //todo: revisar esta condición
             return new ResponseEntity<>("Missing to complete the data", HttpStatus.FORBIDDEN);
         }
+        //verificar que el monto sea mayor que 0
         if (loanApplicationDTO.getAmount()<=0){
             return new ResponseEntity<>("The amount must be greater than 0", HttpStatus.FORBIDDEN);
         }
+        //Verificar que las coutas seran mayores que 0
         if (loanApplicationDTO.getPayments()<=0){
             return new ResponseEntity<>("The payments must be greater than 0", HttpStatus.FORBIDDEN);
         }
+        //Verificar que el préstamo exista
         if (!loanRepository.existsById(loanApplicationDTO.getLoanId())){
             return new ResponseEntity<>("The loan you want to access does not exist", HttpStatus.FORBIDDEN);
         }
+        //Verificar que el monto solicitado no exceda el monto máximo del préstamo
         if (loanApplicationDTO.getAmount()> loan.getMaxAmount()){
             return new ResponseEntity<>("The loan cannot exceed the maximum of "+loan.getMaxAmount(), HttpStatus.FORBIDDEN);
         }
+        //Verificar que la cantidad de cuotas se encuentre entre las disponibles del préstamo
         if (!loan.getPayments().contains(loanApplicationDTO.getPayments())){
             return new ResponseEntity<>("Requested payments are not available", HttpStatus.FORBIDDEN);
         }
+        //Verificar que la cuenta de destino exista
         if (loanApplicationDTO.getToAccountNumber() == null) {
             return new ResponseEntity<>("The selected account does not exist", HttpStatus.FORBIDDEN);
         }
+        //Verificar que la cuenta de destino  sea del cliente autenticado
         if (!client.getAccounts().stream().anyMatch(account1 -> account1.getNumber().equals(loanApplicationDTO.getToAccountNumber()))) {
             return new ResponseEntity<>("The selected account does not belong to you", HttpStatus.FORBIDDEN);
         }
